bdba4a42abaf1fb1bece4eca127ee7ffd4c7233f
JhonMaiza <jhonmaiza@outlook.com>
2023-12-30 01:22:09 -0500

    firt commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import logoContador from './imagenes/logo-contador.png'
+import Contador from './componentes/Contador'
+import Boton from './componentes/Boton'
+import { useState } from 'react';
+
+
+function App() {
+
+  const [numClicks, setNumClicks ] = useState(0); 
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
+
+  return (
+    <div className="App">
+      <div className='titulo'>
+        Contador de Clicks
+      </div>
+      <div className='logo-contenedor'>
+        <img 
+          className='logo-contador'
+          src={logoContador}
+          alt='Logo Contador' />
+      </div>
+      
+      <div className='contenedor-principal'>
+        <Contador
+          numClicks={numClicks}
+        />
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador} />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
